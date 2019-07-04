708f1eec8b3fcce4fa01ff171741a7b3c0d70a51
user130103 <hripsime.b1.y@tumo.org>
2019-07-04 19:10:31 +0400

    hhhh

--- src/app/main/main.page.ts
+++ src/app/main/main.page.ts
@@ -42,8 +42,10 @@ export class Beatbox {
         '6': false,
         '7': false,
         '8': false,
+        '9': false,
+        '10': false
       }, 
-      sounds: new Tone.Player("./mp3/top_1.mp3").toMaster()
+      sound: new Tone.Player("./mp3/top_1.mp3").toMaster()
     },
     second: {
       exp: {
@@ -55,6 +57,8 @@ export class Beatbox {
         '6': false,
         '7': false,
         '8': false,
+        '9': false,
+        '10': false
       },
       sound: new Tone.Player("./mp3/middle_1.mp3").toMaster()
     },
@@ -68,32 +72,42 @@ export class Beatbox {
         '6': false,
         '7': false,
         '8': false,
+        '9': false,
+        '10': false
       },
       sound: new Tone.Player("./mp3/bottom_1.mp3").toMaster()
+    }
+  }
+  tones_1 = {
+    guitar: {
+      exp: {
+        '1': false
+      },
+      sound: new Tone.Player("./mp3/guitarron.mp3").toMaster()
     },
+    horns: {
+      exp: {
+        '1': false
+      },
+      sound: new Tone.Player("./mp3/horns.mp3").toMaster()
+    }
   }
   ind = { count: 0 };
-  playbutton = false;
   playInterval;
-
-
-  
+  playInterval_1;
+  count = 0;
+  count_1 = 0;
+  ind_1 = { count_1: 0 };
   play() {  
-  this.playbutton = !this.playbutton;
-  console.log(this.playbutton+ "++++++++++++++++++++");
     let ind = this.ind;
-    let step = this.ind.count % 4;
+    let ind_1 = this.ind_1;
     let tones = this.tones;
-   let pl = this.playbutton
-   let clInt = this.playInterval
+    let tones_1 = this.tones_1;
     function repeat() {
-      if(pl == false){
-        clearInterval(clInt)
-        }
       for (let tone in tones) {
-        if (ind.count == 8) ind.count = 0;
+        if (ind.count == 10) ind.count = 0;
         let currTone = tones[tone];
-        let count = +"1" + ind.count;
+        let count = + "1" + ind.count;
         console.log(currTone.exp[count])
         if (currTone.exp[count]) {
           Tone.context.resume().then(() => {
@@ -102,19 +116,36 @@ export class Beatbox {
         }
       }
      setTimeout(() => { ind.count++ }, 0);
-
     }
-  
+    this.count++;
+    if(this.count%2!=0){
+        this.playInterval = setInterval(repeat,1000);
+    }
+    else{
+      clearInterval(this.playInterval);
+    }
 
-    this.playInterval = setInterval(repeat,500);
-
-
-
-    // myVar = setInterval(() => { repeat(); }, 500);
-    // if(this.playbutton = false){
-    //   clearInterval(myVar);
-    // }
+    function repeat_1() {
+      for (let tone_1 in tones_1) {
+        if (ind_1.count_1 == 10) ind_1.count_1 = 0;
+        let currTone_1 = tones_1[tone_1];
+        let count_1 = + "1" + ind_1.count_1;
+        console.log(currTone_1.exp[count_1])
+        if (currTone_1.exp[count_1]) {
+          Tone.context.resume().then(() => {
+            currTone_1.sound.start();
+          })
+        }
+      }
+     setTimeout(() => { ind_1.count_1++ }, 0);
+    }
+    this.count_1++;
+    if(this.count_1%2!=0){
+        this.playInterval_1 = setInterval(repeat_1,1000);
+    }
+    else{
+      clearInterval(this.playInterval_1);
+    }
   }
-  
 }
 
